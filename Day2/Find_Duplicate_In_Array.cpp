91e36927441fbe95b88cfb13fe42f321de89284c
= <yashyadao2001@gmail.com>
2022-06-13 23:00:00 +0530

    Day 2

---
+++ Day2/Find_Duplicate_In_Array.cpp
@@ -0,0 +1,19 @@
+int findDuplicate(vector<int>& nums) {
+        int n=nums.size();int s=0,f=0;
+        
+        do{
+            s=nums[s];
+            f=nums[nums[f]];
+        }while(s!=f);
+        s=0;
+        
+        while(s!=f){
+            s=nums[s];
+            f=nums[f];
+        }
+        return s;
+    
+        
+        
+        
+    }
